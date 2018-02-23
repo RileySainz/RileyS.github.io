63d0898967b314cf28b54bd96f9ad6b8d10bd840
Ohm Sainz <ohmsainz@gmail.com>
2018-02-23 09:55:28 -0600

    Calculator for IMC

---
+++ reto90-imc/js/main.js
@@ -0,0 +1,8 @@
+var onReadyDocument = function() {
+	var r90Height = $('#r90Height').val();
+	$('#btnIMC').on('click', function() {
+		console.log('Hello World');
+	});
+}
+
+$(document).on('ready', onReadyDocument);
